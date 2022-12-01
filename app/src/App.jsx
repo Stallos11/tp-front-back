ed855d9b37b02cc79af6cd64b0c3dece834cc19d
Stallos11 <vincentdelev@yahoo.fr>
2022-12-01 18:51:30 +0100

    fix: use VITE ssr implementation

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react';
 
-const getUsers = () => {
+export function App() {
+  const [users, setUsers] = useState([]);
+
+  const getUsers = () => {
   return fetch('http://localhost:8000')
     .then((response) => response.json())
     .then((data) => data);
-};
-
-function App() {
-  const [users, setUsers] = useState([]);
+  };
 
   useEffect(() => {
     getUsers().then((users) => setUsers(users));
@@ -16,9 +16,8 @@ function App() {
   return (
     <div>
       <h1>Users</h1>
-      <ul>{users.length > 0 && users.map((user, i) => <li key={'user-'[i]}>{user.name}</li>)}</ul>
+      <ul>{users.length > 0 && users.map((user, i) => <li key={'user-' +  ++i}>{user.name}</li>)}</ul>
     </div>
   );
 }
 
-export default App;
